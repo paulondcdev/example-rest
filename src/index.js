a8990e1bbacb02dbe63769bdb006a4d4804bbd2c
Paulo Nogueira <paulondcdev@gmail.com>
2017-02-27 06:28:09 -0800

    loading actions in the main module

--- src/index.js
+++ src/index.js
@@ -1,2 +1,22 @@
-const Calc = require('./Calc');
-const app = require('./app');
+const express = require('express');
+const Oca = require('oca');
+
+// loading the registered actions
+require('./Actions');
+
+// creating an express app
+const app = express();
+
+// adding the rest support to the express app
+Oca.restful(app);
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!\n');
+
+  console.log('Sum: http://localhost:3000/sum?x=1&y=10');
+  console.log('Multiply: http://localhost:3000/multiply?x=2&y=2');
+})
